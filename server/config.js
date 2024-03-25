af8b3dda31709341b1ab196c0c0c73d76c531133
mpassava <mpassava@gmail.com>
2024-03-24 23:31:21 -0400

    configured database connection using DefaultAzureCredential

--- server/config.js
+++ server/config.js
@@ -1,35 +1,39 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: `.env.${process.env.NODE_ENV.trim()}`, debug: true });
-// import { DefaultAzureCredential } from "@azure/identity";
+import { DefaultAzureCredential } from "@azure/identity";
 
-// const credential = new DefaultAzureCredential();
+const credential = new DefaultAzureCredential();
+const token = await credential.getToken("https://database.windows.net/.default");
 
 const server = process.env.AZURE_SQL_SERVER;
 const database = process.env.AZURE_SQL_DATABASE;
 const port = parseInt(process.env.AZURE_SQL_PORT);
-// const type = process.env.AZURE_SQL_AUTHENTICATIONTYPE;
-const user = process.env.AZURE_SQL_USER;
-const password = process.env.AZURE_SQL_PASSWORD;
-
-// export const config = {
-//   server,
-//   database,
-//   port,
-//   authentication: {
-//     type
-//   },
-//   options: {
-//     encrypt: true,
-//   },
-// };
+const type = process.env.AZURE_SQL_AUTHENTICATIONTYPE;
+// const user = process.env.AZURE_SQL_USER;
+// const password = process.env.AZURE_SQL_PASSWORD;
 
 export const config = {
   server,
-  port,
   database,
-  user,
-  password,
+  port,
+  authentication: {
+    type: type,
+    options: {
+      token: token.token
+    }
+  },
   options: {
     encrypt: true,
   },
 };
+
+// export const config = {
+//   server,
+//   port,
+//   database,
+//   user,
+//   password,
+//   options: {
+//     encrypt: true,
+//   },
+// };
