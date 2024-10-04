636b6d0481c12386eb1a60f9f374d09a0c48b19d
mpassava <mpassava@gmail.com>
2024-10-03 20:51:23 -0400

    added MaxWidthWrapper component

--- client/workout-app/app/page.tsx
+++ client/workout-app/app/page.tsx
@@ -1,5 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <h1>Workout App</h1>
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Workout Builder{' '}
+          <span className="text-blue-600">V0.1</span>
+        </h1>
+      </div>
+    </MaxWidthWrapper>
   );
 }
