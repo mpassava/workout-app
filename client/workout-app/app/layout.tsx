597beb3fa855f79d89870ab99e163e117dce02ba
mpassava <mpassava@gmail.com>
2024-10-03 23:03:14 -0400

    added button component and header bar icon

--- client/workout-app/app/layout.tsx
+++ client/workout-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import HeaderBar from "@/components/HeaderBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body
-        className={cn("relative h-full font-sans antialiased", inter.className)}
-      >
+      <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+        <HeaderBar />
         <main className="relative flex flex-col min-h-screen">
           <div className="flex-grow flex-1">{children}</div>
         </main>
--- client/workout-app/app/page.tsx
+++ client/workout-app/app/page.tsx
@@ -1,13 +1,17 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
+import { UserPlus } from "lucide-react";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Workout Builder{' '}
-          <span className="text-blue-600">V0.1</span>
+        <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-6xl">
+          Build your <span className="text-primary">gains</span> today.
         </h1>
+        <Button className="mt-10">
+          <UserPlus className="mr-2 h-4 w-4" /> Get Started
+        </Button>
       </div>
     </MaxWidthWrapper>
   );
