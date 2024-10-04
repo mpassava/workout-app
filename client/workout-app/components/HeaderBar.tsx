91c167e25eb2927e5838e8507a123c99e94701d9
mpassava <mpassava@gmail.com>
2024-10-03 23:08:02 -0400

    adjusted header styling and icon size

--- client/workout-app/components/HeaderBar.tsx
+++ client/workout-app/components/HeaderBar.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 
 const HeaderBar = () => {
   return (
-    <MaxWidthWrapper className="flex flex-row min-h-14 py-1 bg-foreground/30">
+    <MaxWidthWrapper className="flex flex-row min-h-14 py-1 bg-foreground/10">
       <div className="flex-grow flex-1 text-left mt-auto">
         <h1 className="text-3xl font-bold tracking-tight text-foreground sm:text-2xl">
-          Workout Builder <span className="text-blue-600">V1.0</span>
+          workout builder <span className="text-blue-600">v1.0</span>
         </h1>
       </div>
       <div className="mt-auto">
         <Link href="#">
-          <UserRound className="h-8 w-8 hover:bg-primary/25 rounded-full" />
+          <UserRound className="h-9 w-9 hover:bg-primary/25 rounded-full" />
         </Link>
       </div>
     </MaxWidthWrapper>
