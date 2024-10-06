77354d24abbd2fb5f74c522c4b58d25861479d84
mpassava <mpassava@gmail.com>
2024-10-05 23:19:46 -0400

    updated call to action

--- client/workout-app/app/page.tsx
+++ client/workout-app/app/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
         <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-6xl">
           Build your <span className="text-primary">gains</span> today.
         </h1>
+        <p className="text-md tracking-tight text-muted-foreground mt-10 w-3/5">
+          Welcome to workout builder, the app that lets you organize, build, and customize your workouts. Try it for free.
+        </p>
         <Button className="mt-10">
           <UserPlus className="mr-2 h-4 w-4" /> Get Started
         </Button>
