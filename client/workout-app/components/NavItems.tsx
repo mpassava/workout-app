930ac312c6bd10973600294f14e544837ac3322f
mpassava <mpassava@gmail.com>
2024-10-05 21:17:39 -0400

    styling

--- client/workout-app/components/NavItems.tsx
+++ client/workout-app/components/NavItems.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 const NavItems = () => (
-  <div className="flex flex-grow flex-1 gap-6 justify-evenly text-lg font-bold tracking-tight text-foreground mt-auto">
-    <div className="flex-grow flex-1 text-center mt-auto">
+  <div className="flex flex-grow flex-1 gap-6 justify-evenly text-md font-bold tracking-tight text-foreground">
+    <div className="flex-grow flex-1 text-center">
       <Link href="#">Builder</Link>
     </div>
-    <div className="flex-grow flex-1 text-center mt-auto">
+    <div className="flex-grow flex-1 text-center">
       <Link href="#">My Workouts</Link>
     </div>
   </div>
--- client/workout-app/components/Navbar.tsx
+++ client/workout-app/components/Navbar.tsx
@@ -3,21 +3,27 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import NavItems from "./NavItems";
 
-const Navbar = () => (
-    <MaxWidthWrapper className="flex flex-row justify-between min-h-12 py-1 bg-foreground/5">
-      <div className="mt-auto flex-grow flex-1">
-        <h1 className="text-2xl font-bold tracking-tight text-foreground sm:text-2xl">
-          workout builder <span className="text-blue-600">v1.0</span>
+const Navbar = () => {
+  return (
+    <>
+    <MaxWidthWrapper className="flex flex-row justify-between min-h-12 py-1 items-center">
+      <div className="flex-grow flex-1">
+        <h1 className="text-2xl font-bold tracking-tight text-foreground">
+          Workout Builder <span className="text-primary">v1.0</span>
         </h1>
       </div>
       <NavItems />
-      <div className="mt-auto flex-grow flex-1 ">
+      <div className="flex-grow flex-1">
         <Link href="#">
           <UserRound className="ml-auto h-8 w-8 hover:bg-primary/25 rounded-full" />
         </Link>
       </div>
     </MaxWidthWrapper>
+    <MaxWidthWrapper>
+      <div className="h-px bg-foreground/5"></div>
+    </MaxWidthWrapper>
+    </>
   );
-
+};
 
 export default Navbar;
