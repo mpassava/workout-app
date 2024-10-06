84e56b5e58c0b438726411c54e3e539d5a7657b0
mpassava <mpassava@gmail.com>
2024-10-06 00:16:22 -0400

    added conditional classNames for active nav item

--- client/workout-app/components/NavItems.tsx
+++ client/workout-app/components/NavItems.tsx
@@ -1,14 +1,35 @@
-import Link from "next/link";
+"use client";
 
-const NavItems = () => (
-  <div className="flex flex-grow flex-1 gap-1 min-w-fit justify-center text-md font-bold tracking-tight text-foreground h-8">
-    <div className="flex-1 text-center mt-auto whitespace-nowrap hover:text-primary hover:text-lg transition-all">
-      <Link href="#">Builder</Link>
+import Link from "next/link";
+import { useState } from "react";
+
+const NavItems = () => {
+  const [active, setActive] = useState<null | string>(null);
+
+  const handleClick = (id: string) => {
+    setActive(id);
+  };
+
+  return (
+    <div className="flex flex-grow flex-1 gap-1 min-w-fit justify-center text-md font-bold tracking-tight text-foreground h-8">
+      <div
+        id="builder"
+        className={`flex-1 text-center mt-auto whitespace-nowrap hover:text-primary hover:text-lg transition-all
+          ${active === "builder" ? "text-primary text-lg" : ""}`}
+        onClick={() => handleClick("builder")}
+      >
+        <Link href="#">Builder</Link>
+      </div>
+      <div
+        id="my-workouts"
+        className={`flex-1 text-center mt-auto whitespace-nowrap hover:text-primary hover:text-lg transition-all
+          ${active === "my-workouts" ? "text-primary text-lg" : ""}`}
+        onClick={() => handleClick("my-workouts")}
+      >
+        <Link href="#">My Workouts</Link>
+      </div>
     </div>
-    <div className="flex-1 text-center mt-auto whitespace-nowrap hover:text-primary hover:text-lg transition-all">
-      <Link href="#">My Workouts</Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default NavItems;
