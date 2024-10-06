9568c89dc548491f0bb79abcf19cbf9a82bb263d
mpassava <mpassava@gmail.com>
2024-10-05 23:08:24 -0400

    more navbar styling

--- client/workout-app/components/NavItems.tsx
+++ client/workout-app/components/NavItems.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 const NavItems = () => (
-  <div className="flex flex-grow flex-1 gap-6 justify-evenly text-md font-bold tracking-tight text-foreground">
-    <div className="flex-grow flex-1 text-center">
+  <div className="flex flex-grow flex-1 gap-1 min-w-fit justify-center text-md font-bold tracking-tight text-foreground h-8">
+    <div className="flex-1 text-center mt-auto whitespace-nowrap hover:text-primary hover:text-lg transition-all">
       <Link href="#">Builder</Link>
     </div>
-    <div className="flex-grow flex-1 text-center">
+    <div className="flex-1 text-center mt-auto whitespace-nowrap hover:text-primary hover:text-lg transition-all">
       <Link href="#">My Workouts</Link>
     </div>
   </div>
--- client/workout-app/components/Navbar.tsx
+++ client/workout-app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { UserRound } from "lucide-react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import NavItems from "./NavItems";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -9,13 +10,13 @@ const Navbar = () => {
     <MaxWidthWrapper className="flex flex-row justify-between min-h-12 py-1 items-center">
       <div className="flex-grow flex-1">
         <h1 className="text-2xl font-bold tracking-tight text-foreground">
-          Workout Builder <span className="text-primary">v1.0</span>
+          <Image src="/logo-1.png" alt="workout builder logo" width={200} height={43}></Image>
         </h1>
       </div>
       <NavItems />
       <div className="flex-grow flex-1">
         <Link href="#">
-          <UserRound className="ml-auto h-8 w-8 hover:bg-primary/25 rounded-full" />
+          <UserRound className="ml-auto h-8 w-8 hover:bg-primary/25 transition-all rounded-full" />
         </Link>
       </div>
     </MaxWidthWrapper>
