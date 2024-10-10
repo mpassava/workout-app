6c0ebadc243e39b73860ad7a85080655312f55d8
mpassava <mpassava@gmail.com>
2024-10-09 21:32:12 -0400

    added better error handling in server.ts

--- client/workout-app/src/server.ts
+++ client/workout-app/src/server.ts
@@ -1,5 +1,5 @@
-import 'dotenv/config'
-import express from "express";
+import "dotenv/config";
+import express, { Request, Response, NextFunction } from "express";
 import chalk from "chalk";
 import morgan from "morgan";
 import debugModule from "debug";
@@ -9,22 +9,36 @@ const PORT = Number(process.env.PORT) || 3000;
 const debug = debugModule("server");
 const app = express();
 
-const start = async () => {
-  debug("starting app");
-  app.use(morgan("tiny"));
+app.use(morgan("tiny"));
 
-  app.use((req, res) => nextHandler(req, res));
+app.use((req: Request, res: Response) => nextHandler(req, res));
 
-  app.route("/api/auth")
-    .get()
-    .post()
-    .patch()
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  debug(`internal error: ${err}`);
+  res.status(500).send("internal server error");
+});
 
-  nextApp.prepare().then(() => {
-    app.listen(PORT, () => {
-      debug(`express listening on port ${chalk.green(PORT)}`);
+const startServer = () => {
+  const server = app.listen(PORT, () => {
+    debug(`express listening on port ${chalk.green(PORT)}`);
+  });
+
+  process.on("SIGINT", () => {
+    debug("Shutting down server...");
+    server.close(() => {
+      debug("Server shut down successfully");
+      process.exit(0);
     });
   });
 };
 
-start();
+(async function start() {
+  debug("starting server...");
+  try {
+    debug("preparing nextApp...");
+    await nextApp.prepare();
+    startServer();
+  } catch (err) {
+    debug(`error preparing nextApp: ${chalk.red(err)}`);
+  }
+})();
