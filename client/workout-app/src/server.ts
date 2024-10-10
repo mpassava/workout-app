e4e4c1a24fa253fff16bc73d2d5b985e58801cc2
mpassava <mpassava@gmail.com>
2024-10-09 20:27:36 -0400

    moved next-server-utils to config folder

--- client/workout-app/src/server.ts
+++ client/workout-app/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import chalk from "chalk";
 import morgan from "morgan";
 import debugModule from "debug";
-import { nextApp, nextHandler } from "./next-server-utils";
+import { nextApp, nextHandler } from "./lib/next-server-utils";
 
 const PORT = Number(process.env.PORT) || 3000;
 const debug = debugModule("server");
@@ -11,7 +11,13 @@ const app = express();
 const start = async () => {
   debug("starting app");
   app.use(morgan("tiny"));
+
   app.use((req, res) => nextHandler(req, res));
+
+  app.route("/api/auth")
+    .get()
+    .post()
+    .patch()
 
   nextApp.prepare().then(() => {
     app.listen(PORT, () => {
